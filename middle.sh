4459cce7249d6972d2e671009db5fc97b51772e5
Samuel Hernandez <samuel.hernandez-1@ou.edu>
2022-06-08 16:01:37 +0000

    added documentation to script and added new test.txt

--- middle.sh
+++ middle.sh
@@ -1,3 +1,6 @@
 # This script will output the middle portions of a file
 
+# usage: middle.sh [name of fiel] [start number of lines] [end number of lines]
+# example middle.sh octane 15 3
+
 head -n "$2" "$1" | tail -n "$3"
